f125d00771da04f81d5dc0da9517b6f6da51ee88
margdaly <margaret.daly@gmail.com>
2023-02-24 11:00:31 -0700

    Time is up

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,14 @@
 class Library
-  attr_reader :name, :books, :authors
+  attr_reader :name, 
+              :books, 
+              :authors, 
+              :publication_dates
 
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @publication_dates = {}
   end
 
   def add_author(author)
@@ -13,5 +17,15 @@ class Library
     end
     @authors << author
     author
+  end
+
+  def publication_time_frame_for(author)
+    dates_of_books = []
+    author.books map do |book|
+      dates_of_books << book.publication_year
+    end
+    @publication_dates[:start] = dates_of_books.min 
+    @publication_dates[:end] = dates_of_books.max
+
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -18,11 +18,24 @@ RSpec.describe Library do
       charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
       jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
       villette = charlotte_bronte.write("Villette", "1853")
-      library.add_author(charlotte_bronte)
-
+      
+      expect(library.add_author(charlotte_bronte)).to eq(charlotte_bronte)
       expect(library.authors.first).to be_a(Author)
       expect(library.books).to eq([jane_eyre, villette])
       expect(library.books.include?('Clifford the Big Red Dog')).to eq(false)
+    end
+  end
+
+  describe '#publication_time_frame_for' do
+    it 'returns a hash' do
+      library = Library.new('Hingham')
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      villette = charlotte_bronte.write("Villette", "1853")
+      library.add_author(charlotte_bronte)
+
+      expect(library.publication_time_frame_for(charlotte_bronte)).to be_a(Hash)
+      expect(library.publication_date)
     end
   end
 end
