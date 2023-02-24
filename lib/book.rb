3580aba47d5e09c725ed3615b5e219832d078b9c
margdaly <margaret.daly@gmail.com>
2023-02-24 09:14:39 -0700

    Add #init, #author, and publication_year to book

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,23 @@
 class Book
+  attr_reader :author_first_name, 
+              :author_last_name, 
+              :title,
+              :publication_date
   
+  def initialize(details)
+    @author_first_name = details[:author_first_name]
+    @author_last_name = details[:author_last_name]
+    @title = details[:title]
+    @publication_date = details[:publication_date]
+  end
+
+  def author
+    author = []
+    author << @author_first_name << @author_last_name
+    author.join(' ')
+  end
+
+  def publication_year
+    @publication_date.split.last
+  end
 end
