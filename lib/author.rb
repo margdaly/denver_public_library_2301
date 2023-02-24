95fe97c8a6eed1589ce1106e9dd064ea8fc27470
margdaly <margaret.daly@gmail.com>
2023-02-24 10:13:33 -0700

    Add #write to author

--- lib/author.rb
+++ lib/author.rb
@@ -12,4 +12,15 @@ class Author
     name << @first_name << @last_name
     name.join(' ')
   end
+
+  def write(title, publication_date)
+    new_book = Book.new({
+      author_first_name: @first_name, 
+      author_last_name: @last_name,
+      title: title, 
+      publication_date: publication_date
+      })
+    @books << new_book
+    new_book
+  end
 end
