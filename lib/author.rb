7fba600916fae47571fb05030ae4c8d77fda3caa
margdaly <margaret.daly@gmail.com>
2023-02-24 09:23:41 -0700

    Add #init and #name to author

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,15 @@
 class Author
+  attr_reader :first_name, :last_name, :books
 
+  def initialize(author_details)
+    @first_name = author_details[:first_name]
+    @last_name = author_details[:last_name]
+    @books = []
+  end
+
+  def name
+    name = []
+    name << @first_name << @last_name
+    name.join(' ')
+  end
 end
